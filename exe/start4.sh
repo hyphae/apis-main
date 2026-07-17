3707a43ce83d7168ecf01d5bb239cf65e65fc7df
Alonso Lopez <alonsl1@uci.edu>
2026-07-16 20:59:15 -0700

    Add logback configuration and update start scripts for logging

--- exe/start4.sh
+++ exe/start4.sh
@@ -4,6 +4,6 @@ CLUSTER_XML=cluster.xml
 if [ "$(uname)" = 'Darwin' ] ; then
 	CLUSTER_XML=cluster-mac.xml
 fi
-java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -Dvertx.hazelcast.config=./$CLUSTER_XML -jar ../target/apis-main-4.5.10-fat.jar -conf ./config4.json -cluster -cluster-host 127.0.0.1
+java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Dlogback.configurationFile=./logback.xml -Dvertx.hazelcast.config=./$CLUSTER_XML -jar ../target/apis-main-4.5.10-fat.jar -conf ./config4.json -cluster -cluster-host 127.0.0.1
 
 echo '... done'
